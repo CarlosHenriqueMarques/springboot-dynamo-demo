6f7d93309b0c7c77860477508f795d20ec4b73a6
Carlos Maia <carlos.marques89@gmail.com>
2020-03-18 12:34:08 -0400

    First commit, base of code

---
+++ src/main/java/com/example/springbootdynamodemo/SpringbootDynamoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdynamodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDynamoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDynamoDemoApplication.class, args);
+	}
+
+}
